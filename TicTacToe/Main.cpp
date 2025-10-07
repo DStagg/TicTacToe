c62c9fcad8415b149c82d647136f9cc33a000aa9
dstagg <staggy11@gmail.com>
2025-10-07 08:03:35 +0100

    Set preprocesser flags to remove the console log from Release.

--- TicTacToe/Main.cpp
+++ TicTacToe/Main.cpp
@@ -1,3 +1,7 @@
+#ifdef _RELEASE_
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#endif
+
 #include <SDL3/SDL.h>
 #include <SDL3_ttf/SDL_ttf.h>
 #include <SDL3_mixer/SDL_mixer.h>
@@ -9,16 +13,16 @@
 #include "src/MenuScene.h"
 #include "src/TTTScene.h"
 
-SDL_Renderer* renderer = nullptr;
-SDL_Window* window = nullptr;
-MIX_Mixer* mixer = nullptr;
+SDL_Renderer* _Renderer = nullptr;
+SDL_Window* _Window = nullptr;
+MIX_Mixer* _Mixer = nullptr;
 
 int main()
 {
 	
 	Timer UpdateClock;
 
-	SDL_CreateWindowAndRenderer("Tic-Tac-Toe", 624, 624, NULL, &window, &renderer);
+	SDL_CreateWindowAndRenderer("Tic-Tac-Toe", 624, 624, NULL, &_Window, &_Renderer);
 
 	if (!TTF_Init())
 	{
@@ -32,9 +36,9 @@ int main()
 		return SDL_APP_FAILURE;
 	}
 
-	mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
+	_Mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
 
-	if (!mixer)
+	if (!_Mixer)
 	{
 		const char* error = SDL_GetError();
 		std::cout << "Couldn't create mixer : " << error << std::endl;
@@ -43,24 +47,25 @@ int main()
 
 	SceneManager SCM;
 
-	SCM.PushScene(new MenuScene(renderer,mixer));
+	SCM.PushScene(new MenuScene(_Renderer,_Mixer));
 
 	UpdateClock.Reset();
 	while (SCM.GetActiveScenePntr() != 0)
 	{
 		SCM.GetActiveScenePntr()->Update(UpdateClock.Lap() / 1000.f);
 
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(_Renderer, 0, 0, 0, 255);
+		SDL_RenderClear(_Renderer);
 		SCM.GetActiveScenePntr()->DrawScreen();
-		SDL_RenderPresent(renderer);
+		SDL_RenderPresent(_Renderer);
 		SCM.CullScenes();
 	}
 	
+	MIX_DestroyMixer(_Mixer);
 	MIX_Quit();
 	TTF_Quit();
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(_Renderer);
+	SDL_DestroyWindow(_Window);
 	SDL_Quit();
 
 	return SDL_APP_SUCCESS;
